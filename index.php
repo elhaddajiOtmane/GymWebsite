b99634b562ac93ba545902b2910ffcf6859db19f
Homen <homennath7638@gmail.com>
2021-09-05 13:46:10 +0530

    Initial Commit

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="stylesheet.css">
+    <?php include 'header.php'; ?>
+    <?php include 'navbar.php'; ?>
+    <script src="https://kit.fontawesome.com/2dfceedea2.js" crossorigin="anonymous"></script>
+    
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-3 col-md-3 text-center" style="height: 50px; background-color: rgb(129, 201, 129);">
+                <p style="color: #000; padding-top: 15px; font-weight: 700;">Important Notice</p>
+
+            </div>
+            <div class="col-sm-9 col-md-9" style="background-color: rgb(120, 129, 102); height: 50px;">
+                <marquee width="100%" direction="left">
+                    <p style="color: #fff; font-weight: 600; padding-top: 10px;">This is a sample scrolling text that has scrolls texts to right.</p>
+                    </marquee>
+            </div>
+        </div>
+       </div> 
+       <div class="container">
+        <div class="row">
+            <div class="col-md-8" style="padding: 20px;">
+                <h4 class="heading1">Shape Your Body</h4>
+                <h1 class="strongHeadMain">BE <span class="strongHead">STRONG</span> AND TRAINING HARD</h1>
+                <p class="textBe">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip.</p>
+                    <button type="button" class="btn btn-warning" id="button1"><span class="spText">Know More</span></button>
+
+                    <div class="container" style="margin-top: 60px;">
+                        <div class="row">
+                            <div class="col-md-4 fa text-center" id="abc">
+                                <i class="fas fa-cog fa-4x"></i>
+                                <h6  class="abcd">Beginners</h6>
+
+                            </div>
+                            <div class="col-md-4 text-center" id="abc">
+                                <i class="fas fa-cogs fa-4x"></i>
+                                <h6  class="abcd">Intermediate</h6>
+                            </div>
+                            <div class="col-md-4 text-center" id="abc">
+                                <i class="fas fa-users-cog fa-4x"></i>
+                                <h6 class="abcd">Advanced</h6>
+                            </div>
+                        </div>
+                    </div>
+            </div>
+            <div class="col-md-4"">
+                <img src="images/bodyBuilder.png" width="100%"  alt="">
+            </div>
+           
+        </div>
+        
+        
+
+    </div>
+</div>
+
+   
+   
+    
+
+
+   
+</body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include 'header.php'; ?>
+    <link rel="stylesheet" href="navStyle.css">
+    <script src="https://kit.fontawesome.com/2dfceedea2.js" crossorigin="anonymous"></script>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <!-- ------------navbar-------- -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <h1 class="brand">abc <span style="color:crimson; font-size: 40pt;"><i>G</i></span>ym</h1>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" id="nav-links" aria-current="page" href="#"><i
+                                class="fas fa-home" style="padding-right: 5px;"></i>Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" id="nav-links" aria-current="page" href="#"><i
+                                class="fas fa-phone-alt" style="padding-right: 5px; "></i>Contact-Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" id="nav-links" aria-current="page" href="#"><i
+                                class="far fa-images" style="padding-right: 5px; "></i>Gallery</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" id="nav-links" aria-current="page" href="#"><i
+                                class="fas fa-bell" style="padding-right: 5px; "></i>Notification</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" id="nav-links" aria-current="page" href="#"><i
+                                class="fas fa-sign-in-alt" style="padding-right: 5px;"></i>Sign in</a>
+                    </li>
+
+
+
+                </ul>
+
+            </div>
+        </div>
+    </nav>
+
+
+
+
+
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
