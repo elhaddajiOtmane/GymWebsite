15c2a5c08ddce7f3796d696ea73fbce6197ce9f7
otmanee7 <39245156+OtmaElHaddaji@users.noreply.github.com>
2022-05-02 11:03:10 +0000

    Update loginPage.php

--- loginPage.php
+++ loginPage.php
@@ -8,10 +8,7 @@ include 'inc/connection.php';
 if (isset($_POST['submit'])) {
     $email = $_POST['email'];
     $pass = $_POST['password'];
-    
-    
     $query = "SELECT * from adminpass where email = '$email' and password='$pass' ";
-    
     $result = mysqli_query($conn, $query);
     $row=mysqli_num_rows($result);
     
@@ -19,17 +16,16 @@ if (isset($_POST['submit'])) {
         $_SESSION['email'] = $_POST['email'];
         header("Location: admin/admindashboard.php");
     } else {
-        `<script> function myFunction() {
+        `<script> function myFunction() 
+        {
             var element = document.getElementById("eror");
             element.classList.add("alert alert-danger mt-3");<script>`;
-            
         }
     }
     if(isset($_SESSION['email'])){
     header("Location: admin/admindashboard.php");
 }
 ?>
-
 <?php include 'blad/navbar.php';?>
     <div class="container">
         <div class="row">
@@ -37,44 +33,30 @@ if (isset($_POST['submit'])) {
                 <img src="images/bodyBuilder2.png" width="100%" alt="">
             </div>
             <div class="col-md-2 "></div>
-
-            <!-- ----------login-mechnism-- -->
+            <!-- ---------login-mechnism-- -->
             <div class="col-md-6 loginPage text-center">
-
                 <div class="loginP">
                     <img src="images/loginLogo.gif" width="45%">
                     <h1>Login</h1>
-
                     <form method="POST" action="loginPage.php" style="width:400px; margin: auto;">
-
                         <div class="form-group row">
-
                             <div class="col-lg-8" style="margin: auto; margin-top: 20px;">
                                 <input type="text" class="form-control bg-light" id="emailid" name="email" placeholder="Enter Email">
-
                             </div>
                         </div>
-
                         <div class="form-group row">
-
                             <div class="col-lg-8" style="margin: auto; margin-top: 20px;">
                                 <input type="password" class="form-control bg-light" id="emailid" name="password" placeholder="Enter Password">
-
                             </div>
                         </div>
-
                         <div class="form-group row">
                             <div class="offset-md-5 col-md-7" style="margin: auto; margin-top: 20px;">
                                 <button type="submit" name="submit" class="btn btn-primary">Login</button>
                             </div>
                             <div id="eror" class="" role="alert"></div>
-
-
                     </form>
 
                 </div>
-
-
 
             </div>
 
